b146c3bc0101e299670325d8da854b305b68b60e
João <jpires.dev@gmail.com>
2024-02-29 01:44:43 +0000

    Update EntityDamageEvent.kt

--- src/main/kotlin/network/roanoke/rutility/modules/battlegodmode/events/EntityDamageEvent.kt
+++ src/main/kotlin/network/roanoke/rutility/modules/battlegodmode/events/EntityDamageEvent.kt
@@ -9,11 +9,17 @@ import network.roanoke.rutility.modules.battlegodmode.BattleGodMode
 class EntityDamageEvent(private val module: BattleGodMode) : ServerLivingEntityEvents.AllowDamage {
 
     override fun allowDamage(entity: LivingEntity?, source: DamageSource?, amount: Float): Boolean {
-        if (module.isEnabled())
-            if (entity is PlayerEntity)
+        if (module.isEnabled()) {
+            if (entity is PlayerEntity) {
                 if (source != null && source.attacker != null && source.attacker is LivingEntity)
                     if (module.playersInBattle.contains(entity.uuid))
                         return false
+            } else if (entity is LivingEntity) {
+                if (source != null && source.attacker != null && source.attacker is PlayerEntity)
+                    if (module.playersInBattle.contains(source.attacker!!.uuid))
+                        return false
+            }
+        }
         return true
     }
 
